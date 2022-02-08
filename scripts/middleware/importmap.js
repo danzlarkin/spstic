738bf0cf39ffda381474ade4383c5ed847377856
larkin-nz <daniel@larkin.nz>
2022-02-09 00:50:02 +1300

    Added dynmic import map reloading client side

--- scripts/middleware/importmap.js
+++ scripts/middleware/importmap.js
@@ -1,11 +1,32 @@
 // Export the import map resolving middleware
 export function importMapMiddleware(config) {
 
-  // Define the imports
-  let imports = {};
+  // Define the import map data
+  const importMap = {
+    imports: {}
+  };
 
   // Check if there is an import map
   if (config.importmap) (async() => {
+
+    // Define the read function
+    const readJSON = async(path) => await Deno.readTextFile(path).then(r => JSON.parse(r));
+
+    // Read the import map and bind the imports
+    importMap.imports = await readJSON(config.importmap));
+
+    // Create a file watcher for the importmap
+    const watcher = Deno.watchFs(config.importmap);
+    
+    // Watch for the events in the directory
+    for await (const event of watcher) {
+
+      // Skip certian events
+      if (['any', 'access'].includes(event.kind)) continue;
+
+      // Read the import map and bind the imports
+      importMap.imports = await readJSON(config.importmap));
+    }
 
     // Read the import map
     const data = await Deno.readTextFile(config.importmap);
@@ -27,10 +48,10 @@ export function importMapMiddleware(config) {
     const esmpath = path.replace('/esm/', '');
 
     // Check if the esmpath is in the import maps
-    if (Object.keys(imports).includes(esmpath)) {
+    if (Object.keys(importMap.imports).includes(esmpath)) {
 
       // Fetch the resource from the esmpath
-      const resource = await fetch(imports[esmpath], {
+      const resource = await fetch(importMap.imports[esmpath], {
         headers: {
           'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36'
         }
